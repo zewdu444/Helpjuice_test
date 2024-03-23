057fb1863ac7c79f74bf12c6de2833e94bf3a4ae
zewdu <zewdu444@gmail.com>
2024-03-23 12:12:13 +0300

    style logo description in footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import ActionCard from "./components/ActionCard";
 import ActionDetail from "./components/ActionDetail";
 import Faq from "./components/Faq";
 import Banner from "./components/Banner";
-
 export default function Home() {
   return (
     <main className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24">
